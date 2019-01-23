e9d39b5f8e0cb8345e2b312cf1895656d0eac88a
samussiah <samussiah@gmail.com>
2019-01-23 18:12:49 -0500

    update data paths

--- test-page/index.js
+++ test-page/index.js
@@ -3,25 +3,25 @@ const dataArray = [
         type: 'DM',
         //domain: 'DM',
         path: 'https://raw.githubusercontent.com/RhoInc/viz-library/master/data/safetyData/SDTM/DM.csv',
-        path: '../../viz-library/data/safetyData/SDTM/DM.csv', // load local data file for performance
+        //path: '../../viz-library/data/safetyData/SDTM/DM.csv', // load local data file for performance
     },
     {
         type: 'AE',
         //domain: 'AE',
         path: 'https://raw.githubusercontent.com/RhoInc/viz-library/master/data/safetyData/SDTM/AE.csv',
-        path: '../../viz-library/data/safetyData/SDTM/AE.csv', // load local data file for performance
+        //path: '../../viz-library/data/safetyData/SDTM/AE.csv', // load local data file for performance
     },
     {
         type: 'BDS',
         //domain: 'LB',
         path: 'https://raw.githubusercontent.com/RhoInc/viz-library/master/data/safetyData/SDTM/LB.csv',
-        path: '../../viz-library/data/safetyData/SDTM/LB.csv', // load local data file for performance
+        //path: '../../viz-library/data/safetyData/SDTM/LB.csv', // load local data file for performance
     },
     {
         type: 'BDS',
         //domain: 'VS',
         path: 'https://raw.githubusercontent.com/RhoInc/viz-library/master/data/safetyData/SDTM/VS.csv',
-        path: '../../viz-library/data/safetyData/SDTM/VS.csv', // load local data file for performance
+        //path: '../../viz-library/data/safetyData/SDTM/VS.csv', // load local data file for performance
     },
 ];
 
