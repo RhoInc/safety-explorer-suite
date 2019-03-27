e8a730b1dc1fd44e1a37ec1973a77beef093760c
samussiah <samussiah@gmail.com>
2019-03-27 16:13:53 -0400

    update data URLs

--- test-page/index.js
+++ test-page/index.js
@@ -2,26 +2,26 @@ const dataArray = [
     {
         type: 'DM',
         //domain: 'DM',
-        path: 'https://raw.githubusercontent.com/RhoInc/viz-library/master/data/safetyData/SDTM/DM.csv',
-        //path: '../../viz-library/data/safetyData/SDTM/DM.csv', // load local data file for performance
+        path: 'https://raw.githubusercontent.com/RhoInc/data-library/master/data/clinical-trials/sdtm/dm.csv',
+        //path: '../../data-library/data/clinical-trials/sdtm/DM.csv', // load local data file for performance
     },
     {
         type: 'AE',
         //domain: 'AE',
-        path: 'https://raw.githubusercontent.com/RhoInc/viz-library/master/data/safetyData/SDTM/AE.csv',
-        //path: '../../viz-library/data/safetyData/SDTM/AE.csv', // load local data file for performance
+        path: 'https://raw.githubusercontent.com/RhoInc/data-library/master/data/clinical-trials/sdtm/ae.csv',
+        //path: '../../data-library/data/clinical-trials/sdtm/AE.csv', // load local data file for performance
     },
     {
         type: 'BDS',
         //domain: 'LB',
-        path: 'https://raw.githubusercontent.com/RhoInc/viz-library/master/data/safetyData/SDTM/LB.csv',
-        //path: '../../viz-library/data/safetyData/SDTM/LB.csv', // load local data file for performance
+        path: 'https://raw.githubusercontent.com/RhoInc/data-library/master/data/clinical-trials/sdtm/lb.csv',
+        //path: '../../data-library/data/clinical-trials/sdtm/LB.csv', // load local data file for performance
     },
     {
         type: 'BDS',
         //domain: 'VS',
-        path: 'https://raw.githubusercontent.com/RhoInc/viz-library/master/data/safetyData/SDTM/VS.csv',
-        //path: '../../viz-library/data/safetyData/SDTM/VS.csv', // load local data file for performance
+        path: 'https://raw.githubusercontent.com/RhoInc/data-library/master/data/clinical-trials/sdtm/vs.csv',
+        //path: '../../data-library/data/clinical-trials/sdtm/VS.csv', // load local data file for performance
     },
 ];
 
